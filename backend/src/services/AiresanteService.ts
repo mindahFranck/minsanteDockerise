f762eebaf4c2e42590bfb036e10b60891ab39a56
mindahFranck <98582484+mindahFranck@users.noreply.github.com>
2025-11-15 21:44:17 +0100

    updated

--- backend/src/services/AiresanteService.ts
+++ backend/src/services/AiresanteService.ts
@@ -1,70 +1,90 @@
-import { BaseService } from "./BaseService"
-import { Airesante } from "../models/Airesante"
+import { BaseService } from "./BaseService";
+import { Airesante } from "../models/Airesante";
 
 export class AiresanteService extends BaseService<Airesante> {
   constructor() {
-    super(Airesante)
+    super(Airesante);
   }
 
   // Override paginate to exclude geom field for performance
-  async paginate(
-    page: number = 1,
-    limit?: number,
-    options?: any
-  ) {
+  async paginate(page: number = 1, limit?: number, options?: any) {
     const modifiedOptions = {
       ...options,
       attributes: {
-        exclude: ['geom'] // Exclure le champ géométrie pour optimiser les performances
-      }
+        exclude: ["geom"], // Exclure le champ géométrie pour optimiser les performances
+      },
     };
     return await super.paginate(page, limit, modifiedOptions);
   }
 
   async getWithRelations(id: number) {
     return await this.findById(id, {
-      attributes: { exclude: ['geom'] },
+      attributes: { exclude: ["geom"] },
       include: [
         { association: "arrondissement" },
         { association: "district" },
         { association: "fosas" },
       ],
-    })
+    });
   }
 
   async getByArrondissement(arrondissementId: number) {
     return await this.findAll({
       where: { arrondissementId },
-      attributes: { exclude: ['geom'] },
+      attributes: { exclude: ["geom"] },
       include: [{ association: "fosas" }],
-    })
+    });
   }
 
   async getByDistrict(districtId: number) {
     return await this.findAll({
       where: { districtId },
-      attributes: { exclude: ['geom'] },
+      attributes: { exclude: ["geom"] },
       include: [{ association: "fosas" }],
-    })
+    });
   }
 
   // Méthodes spécifiques pour la carte (avec geom)
   async getAllForMap() {
     return await this.findAll({
-      attributes: ['id', 'nom_aire', 'code_aire', 'geom', 'districtId']
-    })
+      attributes: [
+        "id",
+        "nom_as",
+        "nom_dist",
+        "code_as",
+        "area",
+        "geom",
+        "districtId",
+      ],
+    });
   }
 
   async getByIdForMap(id: number) {
     return await this.findById(id, {
-      attributes: ['id', 'nom_aire', 'code_aire', 'geom', 'districtId']
-    })
+      attributes: [
+        "id",
+        "nom_as",
+        "nom_dist",
+        "code_as",
+        "area",
+        "geom",
+        "districtId",
+      ],
+    });
   }
 
   async getByDistrictForMap(districtId: number) {
     return await this.findAll({
       where: { districtId },
-      attributes: ['id', 'nom_aire', 'code_aire', 'geom', 'districtId']
-    })
+      attributes: [
+        "id",
+        "nom_as",
+        "nom_dist",
+        "code_as",
+        "area",
+        "geom",
+        "districtId",
+      ],
+    });
   }
 }
